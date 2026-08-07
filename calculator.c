383b353053261ca5d0073d1ac61212c5ffb1458d
sumitbapu <sumit.bapu@gmail.com>
2026-08-08 01:48:14 +0530

    calculator added

---
+++ calculator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=25,b=12;
+    printf("a+b=%d\n",a+b);
+    printf("a-b=%d\n",a-b);
+    printf("a*b=%d\n",a*b);
+    printf("a/b=%d\n",a/b);
+    printf("a^b=%d\n",a^b);
+}
